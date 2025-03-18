070ecf8472fb27bdbd6c9518f42a892d9aaa7c63
bai <bai@baturax.com>
2025-03-18 21:11:12 +0300

    fix vim warn

---
+++ action.sh
@@ -0,0 +1,1 @@
+export XDG_CONFIG_HOME="/data/local/tmp/configs"
--- service.sh
+++ service.sh
@@ -1,1 +1,6 @@
 #empty for a while
+
+CONFIGS_DIR="/data/local/tmp/configs"
+
+mkdir $CONFIGS_DIR
+mkdir $CONFIGS_DIR/vim && touch $CONFIGS_DIR/vim/vimrc
